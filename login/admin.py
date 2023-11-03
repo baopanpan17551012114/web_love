cfba8126e454b4604c76a81387a6584ad20cd63a
baopanpan3 <baopanpan3@jd.com>
2023-11-03 14:28:25 +0800

    first commit

---
+++ login/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+# 注册Model类
+from .models import Question
+
+admin.site.register(Question)
+
+# baopanpan3
+# 1203955829@qq.com
+# zaijian112114
+
---
+++ login/views.py
@@ -0,0 +1,30 @@
+# coding: utf-8
+import json
+from django.shortcuts import render, redirect
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        passowrd = request.POST.get('password')
+        print(username, passowrd)
+        if username == '2022-01-11' and passowrd == '2022-11-05':
+            # return redirect('/index')
+            return render(request, 'heart.html')
+        else:
+            return render(request, 'error.html')
+
+    return render(request, 'login_cool.html')
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def error_deel(request):
+    # return render(request, 'login_cool.html')
+    return redirect('/login_new')
+
+
+def login_new(request):
+    return render(request, 'login_cool_new.html')
