48441077d623a390c24764d847f5461f1c12d570
baopanpan3 <baopanpan3@jd.com>
2023-11-03 18:18:52 +0800

    [feat]add requirements

--- web_love/settings.py
+++ web_love/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(2c(sw$4$6)lkuzy140u+6l3&t*28l2!b%752)&3mf$4+924ci'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
